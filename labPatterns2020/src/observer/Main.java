580bd3ad1f5eaf7788af33dd4bb54d5462da9ec3
jonander <jonander@pop-os.localdomain>
2020-11-03 19:25:47 +0100

    Observer and factory methods finished

--- labPatterns2020/src/observer/Main.java
+++ labPatterns2020/src/observer/Main.java
@@ -3,6 +3,7 @@ package observer;
 import java.util.Observable;
 
 import domain.Covid19Pacient;
+import domain.SymptomFactory;
 
 public class Main {
 
@@ -10,6 +11,18 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		Observable pacient=new Covid19Pacient("aitor", 35, new SymptomFactory());
+		PacientObserverGUI pacientGUI= new PacientObserverGUI (pacient);
+		PacientSymptomGUI frame = new PacientSymptomGUI ((Covid19Pacient) pacient);
+		PacientThermometerGUI p = new PacientThermometerGUI(pacient);
+		Observable pacient2=new Covid19Pacient("Ane", 35, new SymptomFactory());
+		PacientObserverGUI pacientGUI2= new PacientObserverGUI (pacient2);
+		PacientSymptomGUI frame2 = new PacientSymptomGUI ((Covid19Pacient) pacient2);
+		PacientThermometerGUI p2 = new PacientThermometerGUI(pacient2);
+		Observable pacient3=new Covid19Pacient("Maria", 35, new SymptomFactory());
+		PacientObserverGUI pacientGUI3= new PacientObserverGUI (pacient3);
+		PacientSymptomGUI frame3 = new PacientSymptomGUI ((Covid19Pacient) pacient3);
+		PacientThermometerGUI p3 = new PacientThermometerGUI(pacient3);
 
 	}
 
--- labPatterns2020/src/observer/SemaphorGUI.java
+++ labPatterns2020/src/observer/SemaphorGUI.java
@@ -7,16 +7,39 @@ import java.util.Observer;
 
 import javax.swing.JFrame;
 
+import domain.Covid19Pacient;
 
-public class SemaphorGUI extends JFrame {
+
+public class SemaphorGUI extends JFrame  implements Observer{
 	/** stores the associated ConcreteSubject */
-	public SemaphorGUI () {
+	public SemaphorGUI (Observable obs) {
 		setSize(100, 100);
 		setLocation(350,10);
 		Color c=Color.green;
 		getContentPane().setBackground(c);
 		repaint();
 		setVisible(true);
+		obs.addObserver(this);
+	}
+	
+	public void update(Observable o, Object o1) {
+		Covid19Pacient p=(Covid19Pacient)o;
+		// Obtain the current covidImpact to paint
+		int farenheit = (int) p.covidImpact();
+		// temperature gauge update
+		if (farenheit < 5) {
+			Color c=Color.green;
+			getContentPane().setBackground(c);
+			repaint();
+		} else if (farenheit >= 5 && farenheit < 10) {
+			Color c=Color.yellow;
+			getContentPane().setBackground(c);
+			repaint();
+		}else {
+			Color c=Color.red;
+			getContentPane().setBackground(c);
+			repaint();
+		}
 	}
 }     
 
